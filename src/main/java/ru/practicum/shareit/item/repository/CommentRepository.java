110a15926154b163ae746fa1847200378622dccf
TemaJava <101995680+TemaJava@users.noreply.github.com>
2023-05-05 13:55:31 +0300

    Update CommentRepository.java

--- src/main/java/ru/practicum/shareit/item/repository/CommentRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/CommentRepository.java
@@ -10,5 +10,5 @@ import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findCommentsByItemOrderByCreatedDesc(Item item);
+    List<Comment> findByItemOrderByCreatedDesc(Item item);
 }
