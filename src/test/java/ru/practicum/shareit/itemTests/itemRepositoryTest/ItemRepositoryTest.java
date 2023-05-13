7be4da711d7309a5ee1c6b4867904de3b742a588
TemaJava <101995680+TemaJava@users.noreply.github.com>
2023-05-13 21:17:04 +0300

    Codestyle

--- src/test/java/ru/practicum/shareit/itemTests/itemRepositoryTest/ItemRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/itemTests/itemRepositoryTest/ItemRepositoryTest.java
@@ -3,11 +3,8 @@ package ru.practicum.shareit.itemTests.itemRepositoryTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.item.repository.ItemRepository;
-import ru.practicum.shareit.request.repository.RequestRepository;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.repository.UserRepository;
 
