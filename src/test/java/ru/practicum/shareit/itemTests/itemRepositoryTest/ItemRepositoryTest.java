a88022b784041f26aae0c11bd2e9935cc87f03cd
TemaJava <101995680+TemaJava@users.noreply.github.com>
2023-05-13 23:50:18 +0300

    dto tests

--- src/test/java/ru/practicum/shareit/itemTests/itemRepositoryTest/ItemRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/itemTests/itemRepositoryTest/ItemRepositoryTest.java
@@ -29,8 +29,7 @@ public class ItemRepositoryTest {
         itemRepository.save(item2);
         List<Item> listDesc = itemRepository.findAllByString("des");
         List<Item> listName = itemRepository.findAllByString("name");
-
-
+        
         assertEquals(1, listDesc.size());
         assertEquals(2, listName.size());
     }
