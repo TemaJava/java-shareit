16c9a40d9ef67bd874a7a93e74b83321404c7f05
TemaJava <101995680+TemaJava@users.noreply.github.com>
2023-05-14 15:18:58 +0300

    Add files via upload

--- src/test/java/ru/practicum/shareit/bookingTests/bookingServiceTest/BookingServiceTest.java
+++ src/test/java/ru/practicum/shareit/bookingTests/bookingServiceTest/BookingServiceTest.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.PageRequest;
 import ru.practicum.shareit.booking.dto.BookingDtoToResponse;
 import ru.practicum.shareit.booking.mapper.BookingMapper;
 import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.booking.model.State;
 import ru.practicum.shareit.booking.model.Status;
 import ru.practicum.shareit.booking.repository.BookingRepository;
 import ru.practicum.shareit.booking.service.BookingServiceImpl;
@@ -131,7 +132,8 @@ public class BookingServiceTest {
         when(bookingRepository.findByItemOwnerCurrent(anyLong(), any(LocalDateTime.class), any(PageRequest.class)))
                 .thenReturn(List.of(booking));
 
-        List<BookingDtoToResponse> bookingDtoResponses = bookingService.getBookingByOwner(user.getId(), "CURRENT",
+        List<BookingDtoToResponse> bookingDtoResponses = bookingService.getBookingByOwner(user.getId(),
+                String.valueOf(State.CURRENT),
                 Pagination.toPageable(1, 1));
         assertEquals(1, bookingDtoResponses.size());
         assertEquals(1, bookingDtoResponses.get(0).getId());
