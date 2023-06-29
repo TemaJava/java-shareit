c865a78bf217458592663c91d83b830aebb0981f
TemaJava <101995680+TemaJava@users.noreply.github.com>
2023-06-29 15:11:14 +0300

    Update ItemController.java

--- gateway/src/main/java/ru/practicum/shareit/item/controller/ItemController.java
+++ gateway/src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -9,7 +9,6 @@ import ru.practicum.shareit.exception.ValidationException;
 import ru.practicum.shareit.item.client.ItemClient;
 import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.ItemDto;
-import ru.practicum.shareit.validation.Create;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Positive;
